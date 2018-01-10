5d1197694a9a86ab7f07bfebb84a16f81796900d
kylin <975168367@qq.com>
2018-01-10 22:53:20 +0800

    新增username和password的配置项，获取

--- configParseControl.py
+++ configParseControl.py
@@ -4,4 +4,6 @@ import ConfigParser
 config = ConfigParser.ConfigParser()  
 config.read("conf/global.conf")
 class globalParam(object):
-    phatomjsSreenShotDir = config.get('global', 'phatomjsSreenShotDir')  
+    phatomjsSreenShotDir = config.get('global', 'phatomjsSreenShotDir')
+    username = config.get('global', 'username')
+    password = config.get('global', 'password')
